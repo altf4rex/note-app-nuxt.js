f71240b5b0f3258762be0b7476ee7a6c29d7e93d
altf4rex <mr.stereo2002@gmail.com>
2024-11-18 21:56:15 +0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>Welcome to My Customized Nuxt Project!</h1>
+    <p>This is a custom version of the default NuxtWelcome component.</p>
+  </div>
+</template>
+
+
+<style>
+/* Глобальные стили */
+body {
+  margin: 0;
+  padding: 0;
+  background-color: black;
+  color: white;
+}
+</style>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100vh; /* Занимает всю высоту экрана */
+  background-color: black;
+  color: white; /* Белый цвет текста для контраста */
+}
+</style>
